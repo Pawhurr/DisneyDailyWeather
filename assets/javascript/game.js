980b143c97e211155814d1a73345822071692897
Paul Whurr <pawhurr@gmail.com>
2019-03-27 22:42:14 -0400

    beginning framework of psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letterUsed = [];
+
+var winText = document.getElementById("win-text");
+var lossText = document.getElementById("loss-text");
+var userChoiceText = document.getElementById("userChoice-text");
+var guessLeftText = document.getElementById("guessLeft-text");
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    if (userGuess === computerGuess) {
+        wins++; 
+    } else {
+        guessesLeft--;
+        letterUsed.push(userGuess);
+    }
+
+
+
+
+
+
+
+}
+
