2907e43b5ba9b0243c0da1fd02f3033b8fefd8cb
Paul Whurr <pawhurr@gmail.com>
2020-03-16 22:44:44 -0400

    new stuff

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const DWDailyWeather = (function(){
+    const date = new Date();
+    let day;
+    let month;
+    let time;
+
     function init(){
         getWeather("Charlotte,NorthCarolina");
+        todaysDay();
+        currentMonth();
+        currentTime();
+        setInterval(currentTime, 600);
+        $("#dateText").html("<h6>" + day + "</h6>\n<h4>" + month + " " + date.getDate() + "</h4>");
     }
 
     function getWeather(place){
@@ -19,6 +29,29 @@ const DWDailyWeather = (function(){
     
 
           });
+    }
+
+    function todaysDay(){
+        let dayOfWeek = date.getDay();
+        day = dayOfWeek === 1 ? "Monday" : dayOfWeek === 2 ? "Tuesday" : dayOfWeek === 3 ? "Wednesday" : dayOfWeek === 4 ? "Thursday" :
+            dayOfWeek === 5 ? "Friday" : dayOfWeek === 6 ? "Saturday" : "Sunday";
+    }
+
+    function currentMonth(){
+        let currentMonth = date.getMonth();
+        month = currentMonth === 1 ? "February" : currentMonth === 2 ? "March" : currentMonth === 3 ? "April" : currentMonth === 4 ? "May" :
+            currentMonth === 5 ? "June" : currentMonth === 6 ? "July" : currentMonth === 7 ? "August" : currentMonth === 8 ? "September" :
+            currentMonth === 9 ? "October" : currentMonth === 10 ? "November" : currentMonth === 11 ? "December" : "January";
+    }
+
+    function currentTime(){
+        let currentHours = date.getHours();
+        let currentMins = date.getMinutes();
+        let nonMilitary = currentHours > 12 ? currentHours - 12 : currentHours;
+        let ampm = currentHours >= 12 ? "pm" : "am";
+        time = nonMilitary + ":" + currentMins + " " + ampm;
+        $("#timeText").empty();
+        $("#timeText").html("<h1>" + time + "</h1>");
     }
 
     return {
